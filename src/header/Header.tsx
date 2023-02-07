5c93756fdf941e0e0bd745a7dcd86902f95ff827
dell <neispravimayas1@gmail.com>
2023-02-07 19:32:38 +0300

    adaptation completed

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -3,17 +3,21 @@ import s from'./header.module.css';
 import flower from "../pictures/flower.png"
 import circeya from "../pictures/Circeya.png"
 import phone from "../pictures/phone.png"
+import iphone from "../pictures/Iphone.png"
 
 const Header = () => {
+    const windowsSize =  window.screen.availWidth > 400;
     return (
         <div className={s.headerWrapper}>
           <div className={s.headerItem}>
-              <img src={flower} alt=""/>
-              <img src={circeya} alt=""/>
+              <img className={s.flower} src={flower} alt="flower"/>
+              <img className={s.circeya} src={circeya} alt="circeya"/>
           </div>
-            <div className={s.headerItem}>
-              <img src={phone} alt=""/>
-
+            <div className={s.headerItemPhone}>
+                {windowsSize
+                    ? <img className={s.phone} src={phone} alt="phone"/>
+                    : <a href="tel:555-555-5555"><img className={s.iphone} src={iphone}  alt="Call 555-555-5555" /></a>
+                }
           </div>
         </div>
     );
